40f8923abedbf128456a14e7b7f244b47233e007
Abhishek <bhatiaabhishek50@gmail.com>
2020-09-19 01:52:57 +0530

    Updates

---
+++ precache-manifest.220f3f783e86e43a6e8e1854bf019096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effdca8ba977d4e9900c6ef1da971b3b",
+    "url": "/Musical_Instruments_Shop/index.html"
+  },
+  {
+    "revision": "9dcb2411b064496eac79",
+    "url": "/Musical_Instruments_Shop/static/css/main.9c8f4288.chunk.css"
+  },
+  {
+    "revision": "f916454c254e6ae9ac37",
+    "url": "/Musical_Instruments_Shop/static/js/2.69934736.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Musical_Instruments_Shop/static/js/2.69934736.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcb2411b064496eac79",
+    "url": "/Musical_Instruments_Shop/static/js/main.f71db8b3.chunk.js"
+  },
+  {
+    "revision": "e04ed0534b22af43514d",
+    "url": "/Musical_Instruments_Shop/static/js/runtime-main.bec8847f.js"
+  }
+]);
