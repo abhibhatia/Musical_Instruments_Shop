cfbb5f171aa5e9d333e45794720c2d73499c4d2e
Abhishek <bhatiaabhishek50@gmail.com>
2020-09-19 02:08:06 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Musical_Instruments_Shop/precache-manifest.2d159df911956405e5cd20138ae598a0.js"
+  "/Musical_Instruments_Shop/precache-manifest.c040ea71f9aabfa195a3f34403033b61.js"
 );
 
 self.addEventListener('message', (event) => {
